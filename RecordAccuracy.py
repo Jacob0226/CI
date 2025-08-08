dfe654cd1a233da88d37a64a1ee6f4b8eaf9b531
Jacob Chang <jacchang@mi325x8-111.atl1.do.cpe.ice.amd.com>
2025-08-08 09:25:41 +0000

    Save accuracy test result to a json file

--- RecordAccuracy.py
+++ RecordAccuracy.py
@@ -9,8 +9,9 @@ def parse_args():
     parser.add_argument("--engine", type=str, required=True, choices=["vLLM", "SGLang"],
                         help="The name of the engine used")
     parser.add_argument("--model", type=str, required=True, help="The name of the model")
-    parser.add_argument("--acc", type=float, required=False, help="Accuracy number")
-    parser.add_argument("--acc-path", type=str, required=False, help="The path to the accuracy report JSON file.")
+    parser.add_argument("--acc", type=float, required=False, help="(SGLang) Accuracy number")
+    parser.add_argument("--acc-path", type=str, required=False, help="(vLLM) The path to the accuracy report JSON file.")
+    parser.add_argument("--out-json", type=str, required=True, help="Save accuracy number to the file.")
     return parser.parse_args()
 
 def get_accuracy_from_json(file_path):
@@ -41,6 +42,29 @@ def main():
         accuracy_score = get_accuracy_from_json(args.acc_path)
         dataset=os.path.basename(args.acc_path).split('.')[0]
 
+    # Save accuracy number
+    if not os.path.exists(args.out_json):
+        data = {
+            "Accuracy": {
+                f"{args.engine}": {
+                    f"{args.model}": accuracy_score
+                }
+            }
+        }
+    else:
+        with open(args.out_json, "r") as f:
+            data = json.load(f)
+        if "Accuracy" not in data:
+            data["Accuracy"] = {}
+        if args.engine not in data["Accuracy"]:
+            data["Accuracy"][args.engine] = {}
+
+    data["Accuracy"][args.engine][args.model] = accuracy_score
+    with open(args.out_json, "w") as f:
+        json.dump(data, f, indent=4)
+        print(f"Data saved successfully to '{args.out_json}'.")
+
+
     if accuracy_score is not None:
         print("---------- Accuracy Test Report ----------")
         print(f"Engine: {args.engine}")
@@ -58,9 +82,12 @@ if __name__ == "__main__":
 '''
 # vLLM
 python3 RecordAccuracy.py --engine vLLM --model meta-llama/Llama-3.1-8B-Instruct \
-    --acc-path ./outputs/20250804_090128/reports/Llama-3.1-8B-Instruct/gsm8k.json
+    --acc-path ./outputs/20250804_090128/reports/Llama-3.1-8B-Instruct/gsm8k.json \
+    --out-json Result/2025-08-08/Result.json
 # SGLang
-python3 RecordAccuracy.py --engine SGLang --model meta-llama/Llama-3.1-8B-Instruct --acc 0.5
+python3 RecordAccuracy.py --engine SGLang --model meta-llama/Llama-3.1-8B-Instruct \
+    --acc 0.999 --out-json Result/2025-08-08/Result.json
+
 
 
 '''
